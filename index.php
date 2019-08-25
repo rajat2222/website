05a9cc7041e538c489c2602235a5c5ce80bcad32
Rajat kumar <rohit.r.sharma2@gmail.com>
2019-08-25 21:35:47 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,434 @@
+<!DOCTYPE html>
+<!--
+author : Rajat Sharma 
+-->
+<html lang="zxx">
+<head>
+	<title>Rajat Kumar : A visionary man who dream, think and make future how it will be a better place to live.</title>
+	<!-- Meta tag Keywords -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8" />
+	<meta name="keywords" content="Creative Profile Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //Meta tag Keywords -->
+	
+	<!-- Custom-Files -->
+	<link rel="stylesheet" href="css/bootstrap.css"><!-- Bootstrap-Core-CSS -->
+	<link rel="stylesheet" href="css/style.css" type="text/css" media="all" /><!-- Style-CSS -->
+	<link rel="stylesheet" href="css/font-awesome.min.css"><!-- Font-Awesome-Icons-CSS -->
+	<!-- //Custom-Files -->
+	
+	<!-- Web-Fonts -->
+	<link href="//fonts.googleapis.com/css?family=Libre+Franklin:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+	<!-- //Web-Fonts -->
+	
+</head>
+<body>
+
+<div class="wrap" id="home">
+	<!-- header -->
+	<header>
+		<div class="container">
+			<div class="header d-lg-flex">
+				<div class="nav_w3ls mt-lg-3">
+					<nav>
+						<label for="drop" class="toggle mt-lg-0 mt-sm-1"><span class="fa fa-bars" aria-hidden="true"></span></label>
+						<input type="checkbox" id="drop" />
+							<ul class="menu">
+								<li class="active"><a href="index.php">Home</a></li>
+								<li class=""><a href="#aboutme">About me</a></li>
+								<li class="">
+								<!-- First Tier Drop Down -->
+								<label for="drop-2" class="toggle">Personal <span class="fa fa-angle-down" aria-hidden="true"></span> </label>
+								<a href="#">Personal <span class="fa fa-angle-down" aria-hidden="true"></span></a>
+								<input type="checkbox" id="drop-2"/>
+								<ul class="inner-dropdown">
+									<li><a href="#skills">My Skills</a></li>
+									<li class=""><a href="#edu-exp">Education and Experience</a></li>
+								</ul>
+								</li>
+								<li class=""><a href="#projects">Work Gallery</a></li>
+								<li class=""><a href="#contact">Contact Me</a></li>
+							</ul>
+					</nav>
+				</div>
+				<div class="social mt-lg-3 mt-1 ml-auto">
+					<ul>
+						<li class="mr-sm-3 mr-2 icons"><a href="https://www.facebook.com/rohit.r.sharma2"><span class="fa fa-facebook" aria-hidden="true"></span></a></li>
+						<li class="mr-sm-3 mr-2 icons"><a href="https://www.instagram.com/rohit.r.sharma2/"><span class="fa fa-instagram" aria-hidden="true"></span></a></li>
+						<li class="mr-sm-3 mr-2 icons"><a href="#"><span class="fa fa-twitter" aria-hidden="true"></span></a></li>
+						<li class=" icons"><a href="#"><span class="fa fa-pinterest" aria-hidden="true"></span></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- //header -->
+	
+	<!-- logo -->
+	<div class="container my-lg-4 mb-4">
+            <div class="logo text-center"> 
+                <h1><a href="index.php" class="fa fa-diamond"> Mr. Rajat Kumar</a></h1>
+		</div>
+	</div>
+	<!-- //logo -->
+	
+	<!-- container -->
+	<div class="container">
+		<!-- main -->
+		<div class="main">
+			
+			<!-- banner -->
+			<section class="banner">
+				<div class="col-md-12">
+					<div class="banner-info">
+						 <h2 class="heading-banner text-capitalize">Hi, I'm <span>Software Developer.</span></h2>
+						<p class=" mt-3">Software Developer <span>from </span>last 2 years, Developing Software for Personal Use and Client's as well, and working as a freelancer.
+						Now Making a step Ahead to Go through the Challenging Environment of a Company and serving my Nation with the best of my abilities. </p>
+						<a href="#more" class="banner-button btn mt-md-5 mt-3 scroll">Read More </a>
+					</div>
+				</div>
+			</section>
+			<!-- //banner -->
+			
+			<!-- about me -->
+			<section class="banner-bottom" id="more">
+					<div class="inner-sec">
+						<div class="row middle-grids">
+							<div class="col-lg-7 advantage-grid-info">
+								<div class="advantage_left">
+									<h2 class="heading text-capitalize mb-4">More <span></span></h2>
+									<h4>I'm a Intermediate Software Developer </h4>
+									<p class="mb-4">Pursuing B.E. from LNCT Group of Colleges Bhopal(2016-2020 batch) as a Computer Science & Engineering Student, While Facing the hurdles of day to day Scenario. Working With Dedication and learning new Technologies like IoT and Automation, Machine Learning etc. Some Hobbies like Playing Guitar & poetry.</p>
+								
+								    <table><tr><td>								
+								<a style="float:left" href="https://drive.google.com/open?id=1w1w_T6PJe_n0Nc6mU0ua0NFgroF0Tgj6" class="banner-button1 btn">Download Resume</a>
+								</td></tr>
+								<tr><td>
+									<a style="float:left; margin-top:10px;" href="tel:+917007417252" data-rel="popup" data-transition="slideup" class="banner-button2 btn" >Hire Me</a>
+									</td></tr></table>
+								
+
+								</div>
+							</div>
+							<div class="col-lg-5 advantage-grid-info1">
+								<div class="advantage_left1 mt-lg-0 mt-4">
+                                                                    <img src="images/personal.jpg" class="img-fluid" alt="">
+								</div>
+							</div> 
+						</div>
+					</div>
+			</section>
+			<!-- //about me-->
+			
+			<!-- services -->
+			<section class="services" id="aboutme">
+				<div class="inner-sec-layer">
+					<h3 class="heading text-capitalize mb-md-5 mb-4">About me <span></span></h3>
+					<div class="row middle-grids">
+						 
+						
+						<div class="col-lg-4 col-md-6 service-in text-center" 
+						>
+							<div class="card">
+								<div class="card-body">
+									<span class="fa fa-desktop mb-4"></span>
+									<h5 class="card-title text-uppercase">Designing</h5>
+									<p class="card-text">Designing things aren't easier but i make it Easier by learning with my full Effort and Endeavour.</p>
+								</div>
+							</div>
+						</div>
+						 <div class="col-lg-4 col-md-6 service-in text-center mt-md-0 mt-4" >
+							<div class="card">
+								<div class="card-body">
+									<span class="fa fa-pencil-square-o mb-4"></span>
+									<h5 class="card-title text-uppercase">Web-App's</h5>
+                                                                        <p class="card-text">Developed Application like <a href="https://www.github.com/rajat2222/college-android-application"><u>College Android Application</u></a>, Website's like<a href="https://socialmirror.org.in/"<u> E-commerce</u></a> etc.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 service-in text-center mt-md-0 mt-4" >
+							<div class="card">
+								<div class="card-body">
+									<span class="fa fa-pencil-square-o mb-4"></span>
+									<h5 class="card-title text-uppercase">Developer</h5>
+                                                                        <p class="card-text">Working as a Developer from last past 3 years creating website and Android application as a freelancer.</p>
+								</div>
+							</div>
+						</div>
+						 <div class="col-lg-4 col-md-6 service-in text-center mt-md-0 mt-4" style="top:25px">
+							<div class="card">
+								<div class="card-body">
+									<span class="fa fa-pencil-square-o mb-4"></span>
+									<h5 class="card-title text-uppercase">Musician</h5>
+                                                                        <p class="card-text">Playing Guitar and cajon(beatbox) as well, as a hobby but not a profession. Creating own rhythm's on Guitars.</p>
+								</div>
+							</div>
+						</div>
+						 <div class="col-lg-4 col-md-6 service-in text-center mt-md-0 mt-4" style="top:25px">
+							<div class="card">
+								<div class="card-body">
+									<span class="fa fa-pencil-square-o mb-4"></span>
+									<h5 class="card-title text-uppercase">Literature works</h5>
+                                                                        <p class="card-text">Reading and writing Poetry in Hindi & in English, Writing Song lyrics and reading literature works on Science and auto-biographies as well.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-4 col-md-6 service-in text-center mt-lg-0 mt-4" style="top:25px">
+							<div class="card">
+								<div class="card-body">
+									<span class="fa fa-window-restore mb-4"></span>
+									<h5 class="card-title text-uppercase">Dedication</h5>
+                                                                        <p class="card-text">I'm Dedicated to my work, It's not my hobby but once i start's doing something i never get up tired,or give up easily on it,<a href="https://www.github.com/rajat2222"><u> some project's</u></a>.</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- //services -->
+			
+			<!-- skills -->
+			<section class="skills" id="skills">
+				<div class="inner-sec">
+					<h3 class="heading text-capitalize mb-md-5 mb-4">My Skills <span></span></h3>
+					<div class="row middle-grids">
+						<div class="col-lg-6">
+							<div class="skills-left">
+								<p class="card-text mb-4">Primarily JAVA,Android and Php Developer but now having hands on new technologies like IoT and SAP modules.</p>
+								<h5>Android Development - 90%</h5>
+								<p class="mb-4 bg clr-ninghty"></p>
+								<h5>Material Designing - 85%</h5>
+								<p class="mb-4 bg clr-eighty-five"></p>
+								<h5>Web Designing - 70%</h5>
+								<p class="mb-4 bg clr-seventy"></p>
+								<h5>Internet of Things - 50%</h5>
+								<p class="" style="top:45px;width: 50%; background: yellow; height: 8px;"></p>
+								<h5 style="margin-top:25px">SAP module - 25%</h5>
+								<p class="" style="margin-top:5px;width: 25%; background:green; height: 8px;"></p>
+							</div>
+						</div>
+						<div class="col-lg-6 mt-lg-0 mt-4">
+							<div class="row text-center">
+								<div class="col-lg-4 col-sm-4 col-6 count-num-txt mb-4">
+								  <div class="number-w3three-info icon1">
+									<span class="fa fa-briefcase mb-sm-4 mb-2"></span>
+									<h5>0</h5>
+									<h6 class="pt-2">Year's <br> official Experience</h6>
+								  </div>
+								</div>
+								<div class="col-lg-4 col-sm-4 col-6 count-num-txt mb-4">
+								  <div class="number-w3three-info icon2">
+									<span class="fa fa-clone mb-sm-4 mb-2"></span>
+									<h5>10+</h5>
+									<h6 class="pt-2">&nbsp;
+									Projects as <br>a Freelance</h6>
+								  </div>
+								</div>
+								<div class="col-lg-4 col-sm-4 col-6 count-num-txt mb-4">
+								  <div class="number-w3three-info icon3">
+									<span class="fa fa-trophy mb-sm-4 mb-2"></span>
+									<h5>5</h5>
+									<h6 class="pt-2">&nbsp; Unofficial Awards</h6>
+								  </div>
+								</div>
+							</div>
+							<p class="">i am having hands on new Software from last three years and also Pursuing B.E. on Computer Science and Engineering from Rajiv Gandhi Prodyogiki Vishwavidyalaya..</p>
+					  </div>
+						
+						
+					</div>
+				</div>
+			</section>
+			<!-- //skills -->
+			
+			<!-- Projects -->
+			<section class="projects" id="projects">
+				<div class="inner-sec">
+					<h3 class="heading text-capitalize mb-md-5 mb-4">Work Gallery <span></span></h3>
+					<div class="row news-grids text-center">
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal1"><img src="images/r1.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal1"><img src="images/q1.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal1"><img src="images/q2.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal1"><img src="images/q3.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal5"><img src="images/q4.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal5"><img src="images/r2.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal5"><img src="images/r3.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal5"><img src="images/r4.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal5"><img src="images/s00.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal5"><img src="images/s10.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal5"><img src="images/s2001.png" alt="Resource not found" class="img-fluid"></a>
+						</div>
+						<div class="col-md-3 col-6 gal-img">
+							<a href="#gal5"><img src="images/s300.png" alt="Resource not found" class="img-fluid"></a>
+					</div>
+                </div>
+            </section>
+			<!-- //Projects -->
+
+			<!-- tabs -->
+			<section class="edu-exp" id="edu-exp">
+				<div class="inner-sec">
+					<h3 class="heading text-capitalize mb-md-5 mb-4">Education and Experience <span></span></h3>
+					<div class="edu-exp-grids">
+						<div class="tab-main text-center">
+							<input id="tab1" type="radio" name="tabs" class="w3pvt-sm" checked>
+							<label for="tab1"><span class="fa fa-graduation-cap" aria-hidden="true"></span>Education</label>
+							<input id="tab2" type="radio" class="w3pvt-sm" name="tabs">
+							<label for="tab2"><span class="fa fa-briefcase" aria-hidden="true"></span>Experience</label>
+							<section id="content1">
+								<div class="row text-left">
+									<div class="col-lg-6 inner-w3pvt-wrap">
+										<h4>Bachelor Degree</h4>
+										<h6>- Year of Passing- 2016-2020</h6>
+										<p>Pursuing B.E. from LNCT group of Colleges Bhopal M.P.</p>
+									</div>
+									<div class="col-lg-6 inner-w3pvt-wrap">
+										<h4>Intermediate</h4>
+										<h6>- Year of Passing- 2016</h6>
+										<p>Studied from Vashistha Vatsalya Public School Allahabad U.P.</p>
+									</div>
+									<div class="col-lg-6 inner-w3pvt-wrap">
+										<h4>HighSchool</h4>
+										<h6>- Year of Passing- 2013</h6>
+										<p>Studied from Ewing Christian Public School Allahabad U.P. .</p>
+									</div>
+                                                                    
+									<div class="col-lg-6 inner-w3pvt-wrap">
+										<h4>Minor Project</h4>
+                                                                                <h6>- working year 2018-2019 -</h6>
+										<p><a href="https://www.github.com/rajat2222/college-android-application">College Android Application</a></p>
+									</div>
+								</div>
+							</section>
+							<section id="content2">
+								<div class="row text-left">
+								    <div class="col-lg-6 inner-w3pvt-wrap">
+										<h4>SAP Material Management</h4>
+										<h6>-working year- 2019</h6>
+										<p>Working as a Intern in Reliance Industries Limited Refinery division (Jamnagar Gujarat).</p>
+									</div>
+									<div class="col-lg-6 inner-w3pvt-wrap">
+										<h4>Front End Developer</h4>
+										<h6>-working year- 2019</h6>
+										<p>Developing Front-end Using HTML, CSS,XML(Android) & Netbeans GUI (Desktop Applications).</p>
+									</div>
+									<div class="col-lg-6 inner-w3pvt-wrap">
+										<h4>Creative Designer</h4>
+										<h6>-working year- 2015-2016</h6>
+										<p>Designed an Attractive DAM model(HydroElectric Power Plant) for School Exhibition.</p>
+									</div>
+									<div class="col-lg-6 inner-w3pvt-wrap">
+										<h4>Back End Developer</h4>
+										<h6> - working year- 2018-2019</h6>
+										<p>Working as a Backend developer from last year having a good knowledge on PHP,JAVA as well as SQL as well.</p>
+									</div>
+									<div class="col-lg-6 inner-w3pvt-wrap">
+										<h4>Mobile Application Developer</h4>
+										<h6>-working year- 2017-2018</h6>
+										<p>Android Developer from last 2 years Developing app for my learning as well as for client's.</p>
+									</div>
+									<div class="col-lg-6 inner-w3pvt-wrap">
+										<h4>Seeking for more</h4>
+										<h6>- ---- -</h6>
+										<p>Seeking for acquire more knowledge on what i have learnt till now.</p>
+									</div>
+								</div>
+							</section>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- tabs -->
+		</div>
+		<!-- //main -->
+		
+		<!-- contact -->
+		<section class="contact" id="contact">
+			<div class="inner-sec">
+				<h3 class="heading text-capitalize mb-4">Get In Touch <span></span></h3>
+				<form name="contact-form" class="contact-form" method="post" action="#">
+					<div class="row">
+						<div class="col-md-6 con-gd">
+							<div class="form-group">
+								<p>Name *</p>
+								<input type="text" class="form-control" id="name" placeholder="Enter Your Name" name="name" required="">
+							</div>
+							<div class="form-group">
+								<p>Email *</p>
+								<input type="email" class="form-control" id="email" placeholder="Enter Your Email Id" name="email" required="">
+							</div>
+
+						</div>
+						<div class="col-md-6 con-gd">
+							<div class="form-group">
+								<p>Send a Message *</p>
+								<textarea name="Message" class="form-control" placeholder="Enter Your Message" required=""></textarea>
+							</div>
+
+						</div>
+					</div>
+					<div class="form-group mb-0">
+						<button type="submit" class="btn btn-default">Submit</button>
+					</div>
+				</form>
+			</div>
+		</section>
+		<!-- contact -->
+	</div>
+	<!-- //container -->
+	
+	<!-- footer -->
+	<div class="container">
+	<footer class="footer-emp-w3ls">
+		<div class="copy-right-top border-top">
+			<p class="copy-right text-center text-white py-4">&copy; 2019 All Rights Reserved | Design by
+                            <a href="https://www.facebook.com/rohit.r.sharma2"><u><i>Rajat kumar</i></u> (Personal Profile)</a>
+			</p>
+		</div>
+	</footer>
+	</div>
+	<!-- //footer -->
+	
+	<!-- move top -->
+	<div class="move-top text-right">
+		<a href="#home" class="move-top"> 
+			<span class="fa fa-angle-up  mb-3" aria-hidden="true"></span>
+		</a>
+	</div>
+	<!-- move top -->
+
+</div>	
+
+</body>
+</html>
